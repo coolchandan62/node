3674b3f414edacc8ad8b7ba36a1caa2227de95b6
chandan <coolchandan62@gmail.com>
2022-01-15 01:51:40 +0530

    Commit npm package

---
+++ demo.js
@@ -0,0 +1,10 @@
+// var http = require('http');
+// http.createServer(function(req, res) {
+//     res.write("Hello from node js server");
+//     res.end();
+// }).listen(5000)
+
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(10, 20));
