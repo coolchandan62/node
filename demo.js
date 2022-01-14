32e5f895fc9fa575501efef420e7ba86ea839f6b
chandan <coolchandan62@gmail.com>
2022-01-15 02:02:03 +0530

    Commit npm module case senstive

--- demo.js
+++ demo.js
@@ -1,10 +1,6 @@
-// var http = require('http');
-// http.createServer(function(req, res) {
-//     res.write("Hello from node js server");
-//     res.end();
-// }).listen(5000)
-
-function sum(a, b) {
-    return a + b;
-}
-console.log(sum(10, 20));
+var http = require('http');
+var uc = require('upper-case')
+http.createServer(function(req, res) {
+    res.write(uc.upperCase('Hi this is chandan.'));
+    res.end();
+}).listen(5000)
